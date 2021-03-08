299e7755e043f753c43d8dfabb1fe5398acd6546
ParshutinD <71442820+ParshutinD@users.noreply.github.com>
2021-03-08 22:11:34 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const closeButton = document.querySelector(".popup-close");
 const openButton = document.querySelector(".openbutton");
 const popup = document.querySelector(".popup");
+const sendForm = document.querySelector(".sendform");
+const inputEmail = document.querySelector(".feedback-email");
+const inputName = document.querySelector(".feedback-name");
+const inputMessage = document.querySelector(".feedback-text");
 
+// open form
 openButton.onclick = function () {
   popup.classList.remove("popup-hidden");
 };
@@ -9,3 +14,12 @@ closeButton.onclick = function () {
   popup.classList.add("popup-hidden");
 };
 
+
+// send form
+ sendForm.onclick = function () {
+  if (!inputEmail.value || !inputName.value || !inputMessage.value) {
+    popup.classList.remove("popup-shake");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("popup-shake");
+  }
+}; 
